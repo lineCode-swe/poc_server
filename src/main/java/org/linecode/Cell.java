1a01e13bccfd5ed5ec4e1fa4772ae04429b7ec23
Alessandro Chimetto <alessandro.chimetto.it@gmail.com>
2021-02-26 12:10:41 +0100

    Aggiunta classe Cell
    Aggiunto snipped di Grid

---
+++ src/main/java/org/linecode/Cell.java
@@ -0,0 +1,56 @@
+package org.linecode;
+
+public class Cell {
+
+    private final int x;
+    private final int y;
+    private boolean locked = false;
+    private boolean obstacle = false;
+    private boolean unit = false;
+    private boolean poi = false;
+
+    public Cell(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public boolean isLocked() {
+        return locked;
+    }
+
+    public void setLocked(boolean locked) {
+        this.locked = locked;
+    }
+
+    public boolean isObstacle() {
+        return obstacle;
+    }
+
+    public void setObstacle(boolean obstacle) {
+        this.obstacle = obstacle;
+    }
+
+    public boolean isUnit() {
+        return unit;
+    }
+
+    public void setUnit(boolean unit) {
+        this.unit = unit;
+    }
+
+    public boolean isPoi() {
+        return poi;
+    }
+
+    public void setPoi(boolean poi) {
+        this.poi = poi;
+    }
+}
--- src/main/java/org/linecode/MyResource.java
+++ src/main/java/org/linecode/MyResource.java
@@ -8,21 +8,48 @@ import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Root resource (exposed at "myresource" path)
  */
 @Path("myresource")
 public class MyResource {
 
+//    @GET
+//    @Produces(MediaType.APPLICATION_JSON)
+//    public Product getProductInJSON() {
+//
+//        Product product = new Product();
+//        product.setName("iPad 3");
+//        product.setQty(999);
+//
+//        return product;
+//    }
+
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Product getProductInJSON() {
+    public List<Product> returnInt() {
+        List<Product> l = new ArrayList<Product>(3);
 
-        Product product = new Product();
-        product.setName("iPad 3");
-        product.setQty(999);
+        Product a = new Product();
+        a.setName("iPhone");
+        a.setQty(3);
 
-        return product;
+        Product b = new Product();
+        b.setName("Samsung");
+        b.setQty(4);
+
+        Product c = new Product();
+        c.setName("Computer");
+        c.setQty(7);
+
+        l.add(a);
+        l.add(b);
+        l.add(c);
+
+        return l;
     }
 
     @POST
